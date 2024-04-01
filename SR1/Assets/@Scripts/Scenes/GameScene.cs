44b88c774cdba3bc21751f3199e4f6c48890f6aa
seonghoon_kim <hooony1324@gmail.com>
2024-04-01 12:16:12 +0900

    기초 프레임워크 세팅, Hero Control, Monster AI
    - Managers, SceneUI, Excel To Json
    - TileMap, Hero, ObjectManager, MonsterAI

---
+++ SR1/Assets/@Scripts/Scenes/GameScene.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static Define;
+
+public class GameScene : BaseScene
+{
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
+
+        SceneType = EScene.GameScene;
+
+        GameObject map = Managers.Resource.Instantiate("BaseMap");
+        map.transform.position = Vector3.zero;
+        map.name = "@BaseMap";
+
+        Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10, -5, 0));
+        hero.CreatureState = ECreatureState.Move;
+
+        CameraController camera = Camera.main.GetOrAddComponent<CameraController>();
+        camera.Target = hero;
+
+        Managers.UI.ShowBaseUI<UI_Joystick>();
+
+        {
+            Monster monster = Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0));
+            monster.CreatureState = ECreatureState.Idle;
+        }
+
+        return true;
+    }
+
+    public override void Clear()
+    {
+
+    }
+}
