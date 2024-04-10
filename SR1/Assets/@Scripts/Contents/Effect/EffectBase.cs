12070ab1994116000ad3b5648d6d6b696c84d699
seonghoon_kim <hooony1324@gmail.com>
2024-04-10 13:28:44 +0900

    대형 몬스터, AoE
    대형의 Grid 3x3방식 적용

--- SR1/Assets/@Scripts/Contents/Effect/EffectBase.cs
+++ SR1/Assets/@Scripts/Contents/Effect/EffectBase.cs
@@ -6,136 +6,139 @@ using static Define;
 
 public class EffectBase : BaseObject
 {
-	public Creature Owner;
-	public EffectData EffectData;
-	public EEffectType EffectType;
+    public Creature Owner;
+    public SkillBase Skill;
+    public EffectData EffectData;
+    public EEffectType EffectType;
 
-	protected float Remains { get; set; } = 0;
-	protected EEffectSpawnType _spawnType;
-	protected bool Loop { get; set; } = true;
+    protected float Remains { get; set; } = 0;
+    protected EEffectSpawnType _spawnType;
+    protected bool Loop { get; set; } = true;
 
-	public override bool Init()
-	{
-		if (base.Init() == false)
-			return false;
+    public override bool Init()
+    {
+        if (base.Init() == false)
+            return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	public virtual void SetInfo(int templateID, Creature owner, EEffectSpawnType spawnType)
-	{
-		DataTemplateID = templateID;
-		EffectData = Managers.Data.EffectDic[templateID];
+    public virtual void SetInfo(int templateID, Creature owner, EEffectSpawnType spawnType, SkillBase skill)
+    {
+        DataTemplateID = templateID;
+        EffectData = Managers.Data.EffectDic[templateID];
 
-		Owner = owner;
-		_spawnType = spawnType;
+        Skill = skill;
 
-		if (string.IsNullOrEmpty(EffectData.SkeletonDataID) == false)
-			SetSpineAnimation(EffectData.SkeletonDataID, SortingLayers.SKILL_EFFECT);
+        Owner = owner;
+        _spawnType = spawnType;
 
-		EffectType = EffectData.EffectType;
+        if (string.IsNullOrEmpty(EffectData.SkeletonDataID) == false)
+            SetSpineAnimation(EffectData.SkeletonDataID, SortingLayers.SKILL_EFFECT);
 
-		// AoE
-		if (_spawnType == EEffectSpawnType.External)
-			Remains = float.MaxValue;
-		else
-			Remains = EffectData.TickTime * EffectData.TickCount;
+        EffectType = EffectData.EffectType;
 
-		// Duration = EffectData.TickTime * EffectData.TickCount;
-		// Period = EffectData.TickTime;
-	}
+        // AoE
+        if (_spawnType == EEffectSpawnType.External)
+            Remains = float.MaxValue;
+        else
+            Remains = EffectData.TickTime * EffectData.TickCount;
 
-	public virtual void ApplyEffect()
-	{
-		ShowEffect();
-		StartCoroutine(CoStartTimer());
-	}
+        // Duration = EffectData.TickTime * EffectData.TickCount;
+        // Period = EffectData.TickTime;
+    }
 
-	protected virtual void ShowEffect()
-	{
-		if (SkeletonAnim != null && SkeletonAnim.skeletonDataAsset != null)
-			PlayAnimation(0, AnimName.IDLE, Loop);
-	}
+    public virtual void ApplyEffect()
+    {
+        ShowEffect();
+        StartCoroutine(CoStartTimer());
+    }
 
-	protected void AddModifier(CreatureStat stat, object source, int order = 0)
-	{
-		if (EffectData.Amount != 0)
-		{
-			StatModifier add = new StatModifier(EffectData.Amount, EStatModType.Add, order, source);
-			stat.AddModifier(add);
-		}
+    protected virtual void ShowEffect()
+    {
+        if (SkeletonAnim != null && SkeletonAnim.skeletonDataAsset != null)
+            PlayAnimation(0, AnimName.IDLE, Loop);
+    }
 
-		if (EffectData.PercentAdd != 0)
-		{
-			StatModifier percentAdd = new StatModifier(EffectData.PercentAdd, EStatModType.PercentAdd, order, source);
-			stat.AddModifier(percentAdd);
-		}
+    protected void AddModifier(CreatureStat stat, object source, int order = 0)
+    {
+        if (EffectData.Amount != 0)
+        {
+            StatModifier add = new StatModifier(EffectData.Amount, EStatModType.Add, order, source);
+            stat.AddModifier(add);
+        }
 
-		if (EffectData.PercentMult != 0)
-		{
-			StatModifier percentMult = new StatModifier(EffectData.PercentMult, EStatModType.PercentMult, order, source);
-			stat.AddModifier(percentMult);
-		}
-	}
+        if (EffectData.PercentAdd != 0)
+        {
+            StatModifier percentAdd = new StatModifier(EffectData.PercentAdd, EStatModType.PercentAdd, order, source);
+            stat.AddModifier(percentAdd);
+        }
 
-	protected void RemoveModifier(CreatureStat stat, object source)
-	{
-		stat.ClearModifiersFromSource(source);
-	}
+        if (EffectData.PercentMult != 0)
+        {
+            StatModifier percentMult = new StatModifier(EffectData.PercentMult, EStatModType.PercentMult, order, source);
+            stat.AddModifier(percentMult);
+        }
+    }
 
-	public virtual bool ClearEffect(EEffectClearType clearType)
-	{
-		Debug.Log($"ClearEffect - {gameObject.name} {EffectData.ClassName} -> {clearType}");
+    protected void RemoveModifier(CreatureStat stat, object source)
+    {
+        stat.ClearModifiersFromSource(source);
+    }
 
-		switch (clearType)
-		{
-			case EEffectClearType.TimeOut:
-			case EEffectClearType.TriggerOutAoE:
-			case EEffectClearType.EndOfAirborne:
-				Managers.Object.Despawn(this);
-				return true;
+    public virtual bool ClearEffect(EEffectClearType clearType)
+    {
+        Debug.Log($"ClearEffect - {gameObject.name} {EffectData.ClassName} -> {clearType}");
 
-			case EEffectClearType.ClearSkill:
-				// AoE범위 안에 있는경우 해제 X
-				if (_spawnType != EEffectSpawnType.External)
-				{
-					Managers.Object.Despawn(this);
-					return true;
-				}
-				break;
-		}
+        switch (clearType)
+        {
+            case EEffectClearType.TimeOut:
+            case EEffectClearType.TriggerOutAoE:
+            case EEffectClearType.EndOfAirborne:
+                Managers.Object.Despawn(this);
+                return true;
 
-		return false;
-	}
+            case EEffectClearType.ClearSkill:
+                // AoE범위 안에 있는경우 해제 X
+                if (_spawnType != EEffectSpawnType.External)
+                {
+                    Managers.Object.Despawn(this);
+                    return true;
+                }
+                break;
+        }
 
-	protected virtual void ProcessDot()
-	{
+        return false;
+    }
 
-	}
+    protected virtual void ProcessDot()
+    {
+
+    }
 
 
-	protected virtual IEnumerator CoStartTimer()
-	{
-		float sumTime = 0f;
+    protected virtual IEnumerator CoStartTimer()
+    {
+        float sumTime = 0f;
 
-		ProcessDot();
+        ProcessDot();
 
-		while (Remains > 0)
-		{
-			Remains -= Time.deltaTime;
-			sumTime += Time.deltaTime;
+        while (Remains > 0)
+        {
+            Remains -= Time.deltaTime;
+            sumTime += Time.deltaTime;
 
-			// 틱마다 ProcessDotTick 호출
-			if (sumTime >= EffectData.TickTime)
-			{
-				ProcessDot();
-				sumTime -= EffectData.TickTime;
-			}
+            // 틱마다 ProcessDotTick 호출
+            if (sumTime >= EffectData.TickTime)
+            {
+                ProcessDot();
+                sumTime -= EffectData.TickTime;
+            }
 
-			yield return null;
-		}
+            yield return null;
+        }
 
-		Remains = 0;
-		ClearEffect(EEffectClearType.TimeOut);
-	}
+        Remains = 0;
+        ClearEffect(EEffectClearType.TimeOut);
+    }
 }
--- SR1/Assets/@Scripts/Contents/Effect/EffectComponent.cs
+++ SR1/Assets/@Scripts/Contents/Effect/EffectComponent.cs
@@ -6,59 +6,59 @@ using static Define;
 
 public class EffectComponent : MonoBehaviour
 {
-	public List<EffectBase> ActiveEffects = new List<EffectBase>();
-	private Creature _owner;
+    public List<EffectBase> ActiveEffects = new List<EffectBase>();
+    private Creature _owner;
 
-	public void SetInfo(Creature Owner)
-	{
-		_owner = Owner;
-	}
+    public void SetInfo(Creature Owner)
+    {
+        _owner = Owner;
+    }
 
-	public List<EffectBase> GenerateEffects(IEnumerable<int> effectIds, EEffectSpawnType spawnType)
-	{
-		List<EffectBase> generatedEffects = new List<EffectBase>();
+    public List<EffectBase> GenerateEffects(IEnumerable<int> effectIds, EEffectSpawnType spawnType, SkillBase skill)
+    {
+        List<EffectBase> generatedEffects = new List<EffectBase>();
 
-		foreach (int id in effectIds)
-		{
-			string className = Managers.Data.EffectDic[id].ClassName;
-			Type effectType = Type.GetType(className);
+        foreach (int id in effectIds)
+        {
+            string className = Managers.Data.EffectDic[id].ClassName;
+            Type effectType = Type.GetType(className);
 
-			if (effectType == null)
-			{
-				Debug.LogError($"Effect Type not found: {className}");
-				return null;
-			}
+            if (effectType == null)
+            {
+                Debug.LogError($"Effect Type not found: {className}");
+                return null;
+            }
 
-			GameObject go = Managers.Object.SpawnGameObject(_owner.CenterPosition, "EffectBase");			
-			go.name = Managers.Data.EffectDic[id].ClassName;
-			EffectBase effect = go.AddComponent(effectType) as EffectBase;
-			effect.transform.parent = _owner.Effects.transform;
-			effect.transform.localPosition = Vector2.zero;
-			Managers.Object.Effects.Add(effect);
+            GameObject go = Managers.Object.SpawnGameObject(_owner.CenterPosition, "EffectBase");
+            go.name = Managers.Data.EffectDic[id].ClassName;
+            EffectBase effect = go.AddComponent(effectType) as EffectBase;
+            effect.transform.parent = _owner.Effects.transform;
+            effect.transform.localPosition = Vector2.zero;
+            Managers.Object.Effects.Add(effect);
 
-			ActiveEffects.Add(effect);
-			generatedEffects.Add(effect);
+            ActiveEffects.Add(effect);
+            generatedEffects.Add(effect);
 
-			effect.SetInfo(id, _owner, spawnType);
-			effect.ApplyEffect();
-		}
+            effect.SetInfo(id, _owner, spawnType, skill);
+            effect.ApplyEffect();
+        }
 
-		return generatedEffects;
-	}
+        return generatedEffects;
+    }
 
-	public void RemoveEffects(EffectBase effects)
-	{
+    public void RemoveEffects(EffectBase effects)
+    {
 
-	}
+    }
 
-	public void ClearDebuffsBySkill()
-	{
-		foreach (var buff in ActiveEffects.ToArray())
-		{
-			if (buff.EffectType != EEffectType.Buff)
-			{
-				buff.ClearEffect(EEffectClearType.ClearSkill);
-			}
-		}
-	}
+    public void ClearDebuffsBySkill()
+    {
+        foreach (var buff in ActiveEffects.ToArray())
+        {
+            if (buff.EffectType != EEffectType.Buff)
+            {
+                buff.ClearEffect(EEffectClearType.ClearSkill);
+            }
+        }
+    }
 }
